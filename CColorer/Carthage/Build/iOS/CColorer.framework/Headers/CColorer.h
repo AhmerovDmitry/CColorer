edeb9a6eb148e0070385431566282ef6af0b0d2b
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-08-02 14:29:36 +0300

    Bug fix

---
+++ CColorer/Carthage/Build/iOS/CColorer.framework/Headers/CColorer.h
@@ -0,0 +1,18 @@
+//
+//  CColorer.h
+//  CColorer
+//
+//  Created by Дмитрий Ахмеров on 02.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CColorer.
+FOUNDATION_EXPORT double CColorerVersionNumber;
+
+//! Project version string for CColorer.
+FOUNDATION_EXPORT const unsigned char CColorerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CColorer/PublicHeader.h>
+
+
